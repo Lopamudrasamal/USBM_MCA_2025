783b00454917f743d2c97eccfb0568ebe402eca2
Lopamudrasamal <lopamudrasamal2521@gmail.com>
2024-10-22 23:11:00 +0530

    this my mern stack project

---
+++ Frontend/JAVASCRIPT/class1/index.js
@@ -0,0 +1,13 @@
+ let name="sony";
+const age=23;
+var city="BBSR";
+console.log("name="+name);
+console.log ("age="+age);
+console.log("city="+city);
+console.log(address);
+var address="BBSR";
+console.log(address);
+let user=null;
+console.log(typeof null);//object
+console.log(user);
+
